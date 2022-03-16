b2477a5a1e1814d385eb8b4ce50b7968b980cded
Thinh Tran <thinhrom11@gmail.com>
2022-03-17 00:06:28 +0700

    feat(api): add database connection (localhost)

--- api/ormconfig.ts
+++ api/ormconfig.ts
@@ -1,12 +1,15 @@
+import * as dotenv from 'dotenv'
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions'
 
+dotenv.config()
 const configs: MysqlConnectionOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT || 3306,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  synchronize: true,
 }
 
 export default configs
