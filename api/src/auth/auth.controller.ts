74bfbdc50bf29b7a86d9cea1336a5f35826c2053
Thinh Tran <thinhrom11@gmail.com>
2022-05-09 15:02:21 +0700

    feat(api): store access_token in cokkies

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,14 +1,12 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, Req, Res, UseGuards } from '@nestjs/common'
 import { ApiExcludeController } from '@nestjs/swagger'
+import { Response } from 'express'
 import { CreateUserDto } from '../module/users/dto/create-user.dto'
 import { User } from '../module/users/entities/user.entity'
 import { UsersService } from '../module/users/users.service'
 import { AuthService } from './auth.service'
-import { Roles } from './decorators/roles.decorator'
-import { JwtAuthGuard } from './guards/jwt-auth.guard'
+import { jwtConstants } from './constants'
 import { LocalAuthGuard } from './guards/local-auth.guard'
-import { RolesGuard } from './guards/roles.guard'
-import { Role } from './role.enum'
 
 @ApiExcludeController()
 @Controller('auth')
@@ -19,16 +17,28 @@ export class AuthController {
     private readonly authService: AuthService,
   ) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Post('login')
-  login(@Req() req): Promise<{ access_token: string }> {
-    return this.authService.login(req.user)
-  }
-
-  @Roles(Role.ADMIN)
-  @UseGuards(JwtAuthGuard, RolesGuard)
   @Post('register')
   register(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto)
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(
+    @Req() req,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<{ access_token: string }> {
+    const payload = await this.authService.login(req.user)
+    const token = payload.access_token
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      maxAge: jwtConstants.secretExpiration,
+    })
+    return payload
+  }
+
+  @Post('logout')
+  logout(): void {
+    // TODO: implement
   }
 }
